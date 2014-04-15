492ef1bc9f02cfbbbc39e231e3c364508c8bd554
Andrey <mail@ansmirnov.ru>
2014-04-15 16:09:15 +0400

    create models

--- zabbix_map/main/models.py
+++ zabbix_map/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Switch(models.Model):
+	zabbix_id = models.IntegerField(db_index=True)
+	zabbix_name = models.CharField(length=250)
+	parent_id = models.IntegerField(db_index=True)
+	point = models.ForeignKey('Point')
+
+class Point(models.Model):
+	geo_N = models.FloatField()
+	geo_E = models.FloatField()
