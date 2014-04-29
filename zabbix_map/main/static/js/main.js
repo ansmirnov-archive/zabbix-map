f012eeb0e43b44a4fb6e885cdd08ee4ac8dcf12d
Andrey <mail@ansmirnov.ru>
2014-04-29 14:11:14 +0400

    add labels

--- zabbix_map/main/static/js/main.js
+++ zabbix_map/main/static/js/main.js
@@ -5,45 +5,40 @@
  * Time: 13:42
  */
 
-function mapClick(e) {
-    var coord = e.get('coordPosition');
-    var url = '/click/'+coord
+function load_points() {
     jQuery.ajax({
-        url: '/click/'+coord,
+        url: '/items/',
         success: function (data) {
-            for (var i = 0; i < data.points.length; i++) {
-                var point = data.points[i];
-                var arr=point.coords.split(',');
+            for (var i = 0; i < data.items.length; i++) {
+                var point = data.items[i];
                 var myGeoObject = new ymaps.GeoObject({
                     geometry: {
                         type: "Point",// тип геометрии - точка
-                        coordinates: [parseFloat(arr[1]), parseFloat(arr[0])] // координаты точки
+                        coordinates: [parseFloat(point.geo_E), parseFloat(point.geo_N)] // координаты точки
                     }
                 });
                 myMap.geoObjects.add(myGeoObject);
-                myGeoObject.events.add('click', function (e) {labelClick(i)})
-                $('#info').html($('#info').html() + '<div class="item">test</div>');
             }
-            alert(data.points);
         },
         dataType: 'JSON',
         method: "GET"
     })
-}
-
-function labelClick(id){
 
 }
 
-    ymaps.ready(init);
-    var myMap;
+function labelClick(id) {
 
-    function init(){
-        myMap = new ymaps.Map ("map", {
-            center: [55.76, 37.64],
-            zoom: 7
-        });
-        myMap.controls.add('mapTools');
-        myMap.controls.add('zoomControl');
-        myMap.events.add('click', mapClick)
-    }
+}
+
+ymaps.ready(init);
+var myMap;
+
+function init() {
+    myMap = new ymaps.Map("map", {
+        center: [58.05, 38.85],
+        zoom: 12
+    });
+    myMap.controls.add('mapTools');
+    myMap.controls.add('zoomControl');
+    load_points();
+}
--- zabbix_map/main/views.py
+++ zabbix_map/main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render_to_response
+from main import models
 
 
 def home(request):
     return render_to_response('home.html')
 
+
+def items(request):
+    return render_to_response('items.json', {
+        'items': models.Switch.objects.all()
+    })
