c0834de7c11bdd67d3f39ee4aa334d268e52e2ee
Andrey <mail@ansmirnov.ru>
2014-05-26 15:44:25 +0400

    add search_fields

--- zabbix_map/main/admin.py
+++ zabbix_map/main/admin.py
@@ -4,5 +4,11 @@ __email__ = 'mail@ansmirnov.ru'
 from django.contrib import admin
 from main import models
 
-admin.site.register(models.Switch)
-admin.site.register(models.Point)
+class SwitchAdmin(admin.ModelAdmin):
+    search_fields = ['zabbix_name']
+
+class PointAdmin(admin.ModelAdmin):
+    search_fields = ['geo_N', 'geo_E']
+
+admin.site.register(models.Switch, SwitchAdmin)
+admin.site.register(models.Point, PointAdmin)
